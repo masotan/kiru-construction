e382dad8e0090a550331bb25a4aaf72a0d839022
Maso <106080985+masotan@users.noreply.github.com>
2022-07-16 01:58:55 -0400

    d

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './pages', 'index.html'));
+  });
